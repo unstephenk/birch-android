823fecc0778dec039062f427207cfd51c08327e7
Stephen Kuehl <unstephenk@gmail.com>
2026-02-15 10:19:40 +0000

    Theme: fill outline/error colors in Pocket scheme

--- app/src/main/java/com/birch/podcast/theme/Colors.kt
+++ app/src/main/java/com/birch/podcast/theme/Colors.kt
@@ -11,12 +11,22 @@ val pcOnCoral = Color(0xFF121212)
 val pcBgDark = Color(0xFF0B0D10)
 val pcSurfaceDark = Color(0xFF111419)
 val pcSurfaceVariantDark = Color(0xFF171C22)
+val pcOutlineDark = Color(0xFF2A333D)
+val pcOutlineVariantDark = Color(0xFF1F2730)
 val pcOnDark = Color(0xFFE9EEF2)
 val pcOnDarkMuted = Color(0xFFB5BEC7)
+
+val pcErrorDark = Color(0xFFFF6B6B)
+val pcOnErrorDark = Color(0xFF1A0B0B)
 
 // Light theme stays clean and neutral.
 val pcBgLight = Color(0xFFF7F7F8)
 val pcSurfaceLight = Color(0xFFFFFFFF)
 val pcSurfaceVariantLight = Color(0xFFEDEEF0)
+val pcOutlineLight = Color(0xFFCED4DB)
+val pcOutlineVariantLight = Color(0xFFE2E6EB)
 val pcOnLight = Color(0xFF111315)
 val pcOnLightMuted = Color(0xFF4A5158)
+
+val pcErrorLight = Color(0xFFB3261E)
+val pcOnErrorLight = Color(0xFFFFFFFF)
